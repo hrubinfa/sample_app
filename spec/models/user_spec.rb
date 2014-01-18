9c227f72139ab4dc9eaa88b33919ea380900bc31
Harry <harryrubinfalcone@gmail.com>
2014-01-17 20:06:32 -0500

    finished sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe User do
 
   before do
-  	@user = User.new(name: "Example User", email: "user@example.com",
-  						password: "foobar", password_confirmation: "foobar") 
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
-
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) } 
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -108,5 +108,8 @@ describe User do
   	end
   end
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,6 +23,14 @@ describe "signup page" do
       end
     end
 
+    describe "after submission" do
+    	before {click_button submit}
+
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+    end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -35,9 +43,25 @@ describe "signup page" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
 
+      describe "after saving the user" do
+      	before { click_button submit }
+      	let(:user) { User.find_by(email: 'user@example.com')}
+
+      	it { should have_title(user.name) }
+      	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
-    
+   
+    describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
 end
