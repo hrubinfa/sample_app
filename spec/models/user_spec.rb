d782257da148ad7c03f809164b836b42c6bcd0a6
Harry <harryrubinfalcone@gmail.com>
2014-01-21 20:55:06 -0500

    add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,13 @@ describe User do
 it { should respond_to(:admin) }
 it { should respond_to(:microposts) }
 it { should respond_to(:feed) }
+it { should respond_to(:relationships) }
+it { should respond_to(:followed_users) }
+it { should respond_to(:reverse_relationships) }
+it { should respond_to(:followers) }
+it { should respond_to(:following?) }
+it { should respond_to(:follow!) }
+it { should respond_to(:unfollow!) }
 
 
   it { should be_valid }
@@ -33,18 +40,18 @@ it { should respond_to(:feed) }
   end
 
   describe "when name is not present" do
-  	before {@user.name = " " }
-  	it { should_not be_valid}
+    before {@user.name = " " }
+    it { should_not be_valid}
   end
 
   describe "when email is not present" do
-  	before {@user.email = " " }
-  	it { should_not be_valid}
+    before {@user.email = " " }
+    it { should_not be_valid}
   end
 
   describe "when name is too long" do
-  	before { @user.name = "a" * 51 }
-  	it { should_not be_valid }
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
   end
 
   describe "when email format is invalid" do
@@ -69,13 +76,13 @@ it { should respond_to(:feed) }
   end
 
   describe "when email address is already taken" do
-  	before do
-  		user_with_same_email = @user.dup
-  		user_with_same_email.email = @user.email.upcase
-  		user_with_same_email.save
-  	end
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
 
-  	it {should_not be_valid}
+    it {should_not be_valid}
   end
 
   describe "when password is not present" do
@@ -113,13 +120,13 @@ it { should respond_to(:feed) }
   end
 
   describe "email address with mixed case" do
-  	let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
 
-  	it "should be saved as all lower case" do
-  		@user.email = mixed_case_email
-  		@user.save
-  		expect(@user.reload.email).to eq mixed_case_email.downcase
-  	end
+    it "should be saved as all lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "remember token" do
@@ -154,10 +161,45 @@ it { should respond_to(:feed) }
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+        let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+       its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+
+     it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+       describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
